192b7646e38ec6c72424c1fc4789329a12a357cc
RedVxlz <palawhat@gmail.com>
2024-11-25 20:13:11 -0800

    Added some code

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Palawhat Suttama";
+string student_id = "673450473-2";
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id: " + student_id);
